b6ee9914454d856ea6992e525ecd6f39bc25a233
Rogier van Dalen <rcv25@cam.ac.uk>
2015-01-27 17:18:03 +0000

    Add TrackedState, a simple class for testing
    Ignore-this: 530247c5b998f5b7aa2cf85d25ffe6e2
    
    darcs-hash:20150127171803-fa073-a5ec23b6c885a4d09e9bac152193c10085bf4f91

---
+++ test/flipsta/tracked_state.hpp
@@ -0,0 +1,41 @@
+/*
+Copyright 2015 Rogier van Dalen.
+
+This file is part of the Flipsta library.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef FLIPSTA_TEST_TRACKED_STATE_HPP_INCLUDED
+#define FLIPSTA_TEST_TRACKED_STATE_HPP_INCLUDED
+
+#include "utility/test/tracked.hpp"
+
+/**
+A state type that behaves like an integer but also tracks copying, moving, et
+cetera.
+*/
+class TrackedState {
+    utility::tracked <int> value_;
+public:
+    TrackedState (utility::tracked_registry & registry, int value)
+    : value_ (registry, value)
+    { assert (value_.content() >= 0); }
+
+    int value() const { return value_.content(); }
+
+    operator int() const { return value_.content(); }
+};
+
+#endif // FLIPSTA_TEST_TRACKED_STATE_HPP_INCLUDED
