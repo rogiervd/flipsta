aae4cd2ed1b6443ff33059a9906b320dcef2c430
Rogier van Dalen <rcv25@cam.ac.uk>
2015-01-27 19:19:25 +0000

    Fix hash_helper.hpp for newer versions of Boost
    Ignore-this: 177a1e1fc621d357d35c4b64f4c9a741
    
    darcs-hash:20150127191925-fa073-97595e5141171d93aea9b9adec2cea85a6bfdda6

--- include/flipsta/core/hash_helper.hpp
+++ include/flipsta/core/hash_helper.hpp
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /** \file
-Basic handling of automata.
+Provide hash_value for types that Boost does not provide it for.
 */
 
 #ifndef FLIPSTA_CORE_HASH_HELPER_HPP_INCLUDED
@@ -26,16 +26,22 @@ Basic handling of automata.
 
 #include <memory>
 
+#include <boost/version.hpp>
 #include <boost/functional/hash_fwd.hpp>
 
 namespace boost {
 
+    // Support for std::shared_ptr was added in Boost 1.51.0.
+    // For reasons unknown to me, Clang 3.5 does not seem to pick up that
+    // definition.
+#if (BOOST_VERSION < 105100) || defined (__clang__)
     template <class Pointee>
         inline std::size_t hash_value (std::shared_ptr <Pointee> const & p)
     {
         boost::hash <Pointee *> hasher;
         return hasher (p.get());
     }
+#endif
 
 } // namespace boost
 
