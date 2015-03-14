d37cdd39f29bb44cdba2ff1152d1bdc8b11eb8b9
Rogier van Dalen <rcv25@cam.ac.uk>
2015-03-14 11:28:12 +0000

    Introduce detail::SoleStore
    Ignore-this: 1a00a7edf4cbbd8ce5a54abef768fac2
    This keeps track a single object for every value.
    
    darcs-hash:20150314112812-fa073-63bc1b90a23592f2600447ac7ab6e8ed2df76438

---
+++ include/flipsta/detail/sole.hpp
@@ -0,0 +1,365 @@
+/*
+Copyright 2015 Rogier van Dalen.
+
+This file is part of the Flipsta library.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef FLIPSTA_DETAIL_SOLE_HPP_INCLUDED
+#define FLIPSTA_DETAIL_SOLE_HPP_INCLUDED
+
+#include <cassert>
+#include <memory>
+#include <type_traits>
+
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/mem_fun.hpp>
+
+#include <boost/functional/hash.hpp>
+
+namespace flipsta { namespace detail {
+
+    template <class Value> class SoleStore;
+
+    struct SoleBaseTag {};
+
+    /** \brief
+    Base class for objects to be stored in \ref SoleStore that require access
+    to other objects in the store.
+
+    This makes sure that the same store is used (and saves some memory).
+
+    Sole needs access to the conversion from and to Value.
+    The destructor is not virtual, so it is not safe to cast \a Value to
+    \a SoleBase in general.
+    The best way of ensuring this is to privately derive from this class, and
+    to make it a friend.
+    */
+    template <class Value> class SoleBase : public SoleBaseTag {
+    protected:
+        typedef SoleStore <Value> Store;
+
+    private:
+        Store * store_;
+        bool inStore_;
+
+        void setInStore() { inStore_ = true; }
+
+        friend class SoleStore <Value>;
+
+        static std::shared_ptr <Value> construct (
+            Store & store, Value const & value)
+        {
+            // We must be inserting into the correct store.
+            assert (value.store_ == &store);
+            auto sole = std::make_shared <Value> (value);
+            sole->setInStore();
+            return std::move (sole);
+        }
+
+        static std::shared_ptr <Value> construct (
+            Store & store, Value && value)
+        {
+            // We must be inserting into the correct store.
+            assert (value.store_ == &store);
+            auto sole = std::make_shared <Value> (std::move (value));
+            sole->setInStore();
+            return std::move (sole);
+        }
+
+    protected:
+        /** \brief
+        Initialise with a pointer to the SoleStore object to use.
+        */
+        SoleBase (Store * store) : store_ (store), inStore_ (false) {}
+
+        /** \brief
+        Return a pointer to the store that is used.
+        */
+        Store * store() const { return store_; }
+
+    public:
+        ~SoleBase() {
+            if (inStore_)
+                store_->removePointer (static_cast <Value const *> (this));
+        }
+    };
+
+    /** \brief
+    Class used internally by SoleStore if Value does not derive from SoleBase.
+    */
+    template <class Value> class SoleValue {
+        Value value_;
+
+        typedef SoleStore <Value> Store;
+        Store & store_;
+
+    public:
+        SoleValue (Store & store, Value const & value)
+        : value_ (std::move (value)), store_ (store) {}
+
+        SoleValue (Store & store, Value && value)
+        : value_ (value), store_ (store) {}
+
+        ~SoleValue() { store_.removePointer (&value_); }
+
+        static std::shared_ptr <Value const> construct (
+            Store & store, Value const & value)
+        {
+            auto sole = std::make_shared <SoleValue> (store, value);
+            // Produce an aliasing pointer to the value.
+            return std::shared_ptr <Value const> (
+                std::move (sole), &sole->value_);
+        }
+
+        static std::shared_ptr <Value const> construct (
+            Store & store, Value && value)
+        {
+            auto sole = std::make_shared <SoleValue> (store, std::move (value));
+            // Produce an aliasing pointer to the value.
+            return std::shared_ptr <Value const> (
+                std::move (sole), &sole->value_);
+        }
+    };
+
+    /** \brief
+    This contains a weak_ptr which should remain able to be converted into a
+    shared_ptr at all times: as soon as the object is destructed, this reference
+    is first destructed.
+
+    Its hash is computed as the hash of the object that it points to.
+    */
+    template <class Value> class WeakPtr {
+        std::weak_ptr <Value const> object_;
+        // This remains even when the object goes out of scope.
+        Value const * rawPointer_;
+
+    public:
+        WeakPtr (std::shared_ptr <Value const> const & object)
+        : object_ (object), rawPointer_ (object.get()) {}
+
+        /** \brief
+        Get a shared_ptr owning the object.
+
+        This should work until the object gets deleted.
+        */
+        std::shared_ptr <Value const> get() const { return object_.lock(); }
+
+        /** \brief
+        Return a reference to the object.
+
+        This should work until the object gets deleted.
+        */
+        Value const & value() const {
+            // Check that the object exists.
+            assert (!object_.expired());
+            return *rawPointer_;
+        }
+
+        Value const * rawPointer() const { return rawPointer_; }
+    };
+
+    template <class Value> std::size_t hash_value (WeakPtr <Value> const & p) {
+        boost::hash <Value> hasher;
+        return hasher (p.value());
+    }
+
+    template <class Value> bool
+        operator== (WeakPtr <Value> const & left, WeakPtr <Value> const & right)
+    { return left.rawPointer() == right.rawPointer(); }
+
+    struct EqualToWeakPtr {
+        template <class Value>
+            bool operator() (Value const & left, WeakPtr <Value> const & right)
+            const
+        { return left == right.value(); }
+
+        template <class Value>
+            bool operator() (WeakPtr <Value> const & left, Value const & right)
+            const
+        { return left.value() == right; }
+    };
+
+    /** \brief
+    Keep track of unique objects for each value.
+
+    When a new value is added, it is checked whether it is already in the store,
+    in which case a std::shared_ptr to the previously stored value is returned.
+    When the last std::shared_ptr to the value is destructed, the value is
+    destructed and removed from the store.
+
+    All std::shared_ptr objects to values in the store must therefore be
+    destructed before the store itself is.
+
+    It is possible to add a particular std::shared_ptr to the store if the value
+    is not in the store yet.
+    This can be useful for adding singleton pointers.
+    Such a pointer must be explicitly removed from the store when it is
+    destructed.
+
+    Values must have support for Boost.Hash, and equality must be defined.
+
+    Sometimes objects need to hold pointers to other objects in the same store
+    In that case, the objects require access to the store themselves, and then
+    they can be derived from SoleBase so they can use the pointer to the store
+    which is kept anyway.
+    Note that objects should not keep a std::shared_ptr to themselves, directly
+    or indirectly; as usual, a weak_ptr should be used to break the cycle.
+
+    This is not guaranteed to be thread-safe.
+
+    \todo A number of optimisations would be possible.
+    Areas to think of are:
+    This class makes use of std::shared_ptr and its facilities.
+    A custom pointer could save memory.
+    For example, the weak_ptr is not really necessary if you believe that the
+    program logic is sound.
+    SoleBase could combine its boolean and the pointer to the store in one word.
+    Hashes may be computed more often than necessary.
+    */
+    template <class Value> class SoleStore {
+        typedef WeakPtr <Value> Pointer;
+
+        /*
+        Keep objects, indexed by:
+        1. The actual value that is pointed to.
+        2. The position of the sole value, so the object can remove itself.
+        */
+        typedef boost::multi_index_container <
+            Pointer,
+            boost::multi_index::indexed_by <
+                boost::multi_index::hashed_unique <
+                    boost::multi_index::identity <Pointer>>,
+                boost::multi_index::hashed_unique <
+                    boost::multi_index::const_mem_fun <
+                        Pointer, Value const *, &Pointer::rawPointer>>
+            >> Objects;
+
+        Objects objects_;
+
+        /**
+        The actual type of object that will be allocated.
+        */
+        typedef typename std::conditional <
+                std::is_base_of <SoleBaseTag, Value>::value,
+                SoleBase <Value>, SoleValue <Value>>::type
+            SoleType;
+
+    public:
+        SoleStore() {}
+
+        ~SoleStore() {
+            // All objects must have gone from memory or they'll try and remove
+            // themselves when they are deleted.
+            assert (objects_.empty());
+        }
+
+        /** \brief
+        Retrieve the sole object with value \a value.
+
+        If an object with this value is already in the store, return a pointer
+        to that object.
+        If not, return a pointer to a newly allocated object.
+        */
+        std::shared_ptr <Value const> get (Value const & value) {
+            auto existing = objects_.find (value,
+                boost::hash <Value>(), EqualToWeakPtr());
+            if (existing != objects_.end())
+                return existing->get();
+
+            // The value is not in the store yet; insert it.
+            auto sole = SoleType::construct (*this, value);
+            Pointer newElement (sole);
+            // Note that this insertion involves a mere copy of newElement into
+            // another place in memory.
+            // No objects will therefore be deleted, and no objects will
+            // therefore attempt to remove themselves from the store.
+            objects_.insert (std::move (newElement));
+
+            return sole;
+        }
+
+        // Rvalue reference.
+        std::shared_ptr <Value const> get (Value && value) {
+            auto existing = objects_.find (value,
+                boost::hash <Value>(), EqualToWeakPtr());
+            if (existing != objects_.end())
+                return existing->get();
+
+            // The value is not in the store yet; insert it.
+            auto sole = SoleType::construct (*this, std::move (value));
+            Pointer newElement (sole);
+            objects_.insert (std::move (newElement));
+
+            return sole;
+        }
+
+        /** \brief
+        Force the pointer for the object in store to be a specific pointer.
+
+        This can be useful for singleton values.
+        These usually must be removed from the store explicitly.
+
+        \pre No object with this value is already in the store.
+        */
+        void set (std::shared_ptr <Value const> pointer) {
+            // The value must not be in the store yet.
+            assert (objects_.find (*pointer,
+                boost::hash <Value>(), EqualToWeakPtr()) == objects_.end());
+
+            objects_.insert (std::move (pointer));
+        }
+
+        /** \brief
+        Remove the object at the pointer from the store.
+        */
+        void remove (std::shared_ptr <Value const> pointer) {
+            this->removePointer (pointer.get());
+        }
+
+    private:
+        friend class SoleBase <Value>;
+        friend class SoleValue <Value>;
+
+        /** \brief
+        Remove the object from the store.
+
+        This is called by the destructor of SoleBase or SoleValue.
+        This may seem dangerous for two reasons:
+        1.  A weak_ptr to an object will be destructed while the shared_ptr and
+            the object itself is being destructed.
+            However, this is a way of implementing std::enable_shared_from_this
+            so this must be supported.
+        2.  What if this is called in the middle of another operation on
+            objects_?
+            It will not.
+            This is simply because this class performs mutating operations on
+            objects_ in only two places.
+            The one here only affects the shared_ptr control block.
+            The one in get() does not affect anything.
+        So this is actually safe.
+        */
+        void removePointer (Value const * pointer) {
+           auto numberDeleted = objects_.template get <1>().erase (pointer);
+           assert (numberDeleted == 1);
+           (void) numberDeleted;
+        }
+
+    };
+
+}} // namespace flipsta::detail
+
+#endif // FLIPSTA_DETAIL_SOLE_HPP_INCLUDED
---
+++ test/flipsta/detail/test-sole.cpp
@@ -0,0 +1,256 @@
+/*
+Copyright 2015 Rogier van Dalen.
+
+This file is part of the Flipsta library.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define BOOST_TEST_MODULE test_core_sole
+#include "utility/test/boost_unit_test.hpp"
+
+#include "flipsta/detail/sole.hpp"
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <ostream>
+
+#include <boost/functional/hash.hpp>
+
+#include "range/for_each_macro.hpp"
+#include "range/std/container.hpp"
+
+using flipsta::detail::SoleStore;
+using flipsta::detail::SoleBase;
+
+using range::view;
+using range::empty;
+using range::chop_in_place;
+
+/**
+Class that keeps an int, but compares equal if (i % 5) is equal.
+*/
+struct IntModulo5 {
+    int i;
+
+    IntModulo5 (int i) : i (i) {}
+
+    bool operator == (IntModulo5 const & other) const
+    { return (this->i % 5) == (other.i % 5); }
+};
+
+
+std::size_t hash_value (IntModulo5 const & i)
+{ return std::size_t (i.i % 5); }
+
+/**
+State that can have other states following it, and has a symbol associated with
+it.
+The hash value depends on the symbol, but not on whether it is final.
+*/
+class SimpleState : SoleBase <SimpleState> {
+    typedef SoleBase <SimpleState> SoleType;
+    friend class SoleBase <SimpleState>;
+
+    char symbol_;
+    bool final_;
+    std::vector <std::shared_ptr <SimpleState const>> successors_;
+
+    void addSuccessor_ (SimpleState const & s) {
+        successors_.push_back (store()->get (s));
+    }
+
+public:
+    SimpleState (Store * store, char symbol, bool final = false)
+    : SoleType (store), symbol_ (symbol), final_ (final) {}
+
+    char symbol() const { return symbol_; }
+
+    bool final() const { return final_; }
+
+    void addSuccessor (SimpleState const & s) const {
+        // By making this mutable, we can test that states are actually shared.
+        // Not normally recommended.
+        const_cast <SimpleState *> (this)->addSuccessor_ (s);
+    }
+
+    void printSequences (std::ostream & os, std::string soFar) const {
+        std::string current = soFar + symbol();
+        if (final_)
+            std::cout << current << '\n';
+        RANGE_FOR_EACH (successor, successors_)
+            successor->printSequences (os, current);
+    }
+
+    void appendSequences (
+        std::vector <std::string> & sequences, std::string soFar) const
+    {
+        std::string current = soFar + symbol();
+        if (final_)
+            sequences.push_back (current);
+        RANGE_FOR_EACH (successor, successors_)
+            successor->appendSequences (sequences, current);
+    }
+};
+
+bool operator== (SimpleState const & left, SimpleState const & right) {
+    return left.symbol() == right.symbol() && left.final() == right.final();
+}
+
+std::size_t hash_value (SimpleState const & s) {
+    boost::hash <char> hasher;
+    return hasher (s.symbol());
+}
+
+BOOST_AUTO_TEST_SUITE(test_suite_sore)
+
+BOOST_AUTO_TEST_CASE (test_sole_simple) {
+    SoleStore <int> ints;
+
+    {
+        auto sole1 = ints.get (1);
+        BOOST_CHECK_EQUAL (*sole1, 1);
+        BOOST_CHECK (ints.get (1) == sole1);
+
+        // minusOne is a special object, which is explicitly added and removed.
+        auto minusOne = std::make_shared <int> (-1);
+        ints.set (minusOne);
+
+        // This is not allowed: 1 is already in the store.
+        // auto one = std::make_shared <int> (1);
+        // ints.set (one);
+
+        auto sole7 = ints.get (7);
+        BOOST_CHECK_EQUAL (*sole7, 7);
+        BOOST_CHECK (ints.get (7) == sole7);
+
+        int i10 = 10;
+        auto sole10 = ints.get (i10);
+        std::weak_ptr <int const> w (sole10);
+        BOOST_CHECK (!w.expired());
+        sole10.reset();
+        // The SoleStore does not hold any shared_ptr itself.
+        // The object must therefore have been destructed.
+        BOOST_CHECK (w.expired());
+
+        ints.remove (minusOne);
+    }
+}
+
+// Multiple objects, some of which compare equal.
+BOOST_AUTO_TEST_CASE (test_flipsta_multiple) {
+    IntModulo5 a (4);
+    IntModulo5 a2 (4);
+    IntModulo5 b (9);
+    IntModulo5 c (3);
+    IntModulo5 d (7);
+
+    {
+        SoleStore <IntModulo5> store;
+
+        std::shared_ptr <IntModulo5 const> aPtr = store.get (a);
+        BOOST_CHECK_EQUAL (aPtr->i, 4);
+        std::shared_ptr <IntModulo5 const> a2Ptr = store.get (a2);
+        BOOST_CHECK_EQUAL (a2Ptr->i, 4);
+
+        BOOST_CHECK (aPtr == a2Ptr);
+
+        std::shared_ptr <IntModulo5 const> bPtr = store.get (b);
+        // Instead of b, a pointer to a copy of a must have been returned,
+        // because they compare equal.
+        BOOST_CHECK (bPtr == aPtr);
+        BOOST_CHECK_EQUAL (bPtr->i, 4);
+
+        std::shared_ptr <IntModulo5 const> cPtr = store.get (c);
+        BOOST_CHECK_EQUAL (cPtr->i, 3);
+
+        std::shared_ptr <IntModulo5 const> dPtr = store.get (d);
+        BOOST_CHECK_EQUAL (dPtr->i, 7);
+
+        std::weak_ptr <IntModulo5 const> cWeakPtr = cPtr;
+        cPtr.reset();
+        // The copy of c is not alive anymore.
+        BOOST_CHECK (cWeakPtr.lock() == std::shared_ptr <IntModulo5 const>());
+
+        // Add it again.
+        cPtr = store.get (c);
+        BOOST_CHECK_EQUAL (cPtr->i, 3);
+    }
+}
+
+// Derive from SoleBase.
+BOOST_AUTO_TEST_CASE (test_sole_derived) {
+    SoleStore <SimpleState> states;
+
+    auto singletonQ = std::make_shared <SimpleState> (nullptr, 'q');
+
+    states.set (singletonQ);
+
+    {
+        auto soleA = states.get (SimpleState (&states, 'a'));
+        BOOST_CHECK_EQUAL (soleA->symbol(), 'a');
+        BOOST_CHECK (states.get (SimpleState (&states, 'a')) == soleA);
+        BOOST_CHECK (states.get (SimpleState (&states, 'a', true)) != soleA);
+
+        SimpleState qState (&states, 'q');
+        auto soleQ = states.get (qState);
+        BOOST_CHECK_EQUAL (singletonQ, soleQ);
+
+        auto bFinal = states.get (SimpleState (&states, 'b', true));
+        auto bNonFinal = states.get (SimpleState (&states, 'b', false));
+        BOOST_CHECK_EQUAL (bFinal->symbol(), 'b');
+        BOOST_CHECK (
+            states.get (SimpleState (&states, 'b')) == bNonFinal);
+        BOOST_CHECK (states.get (SimpleState (&states, 'b', true)) == bFinal);
+
+        soleA->addSuccessor (SimpleState (&states, 'b'));
+        soleA->addSuccessor (SimpleState (&states, 'c'));
+        soleA->addSuccessor (SimpleState (&states, 'b', true));
+
+        // Change B and C in hindsight. (Not normally recommended.)
+        bFinal->addSuccessor (SimpleState (&states, 'c'));
+        bNonFinal->addSuccessor (SimpleState (&states, 'd', true));
+        states.get (SimpleState (&states, 'c'))->addSuccessor (
+            SimpleState (&states, 'd', true));
+
+        // Printing the sequences is useful to see what is happening.
+        bFinal->printSequences (std::cout, "From bFinal: ");
+        soleA->printSequences (std::cout, "From soleA: ");
+
+        {
+            std::vector <std::string> sequences;
+            bFinal->appendSequences (sequences, "");
+
+            auto s = view (sequences);
+            BOOST_CHECK_EQUAL (chop_in_place (s), "b");
+            BOOST_CHECK_EQUAL (chop_in_place (s), "bcd");
+            BOOST_CHECK (empty (s));
+        }
+        {
+            std::vector <std::string> sequences;
+            soleA->appendSequences (sequences, "");
+
+            auto s = view (sequences);
+            BOOST_CHECK_EQUAL (chop_in_place (s), "abd");
+            BOOST_CHECK_EQUAL (chop_in_place (s), "acd");
+            BOOST_CHECK_EQUAL (chop_in_place (s), "ab");
+            BOOST_CHECK_EQUAL (chop_in_place (s), "abcd");
+            BOOST_CHECK (empty (s));
+        }
+    }
+    states.remove (singletonQ);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
